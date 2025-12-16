434e6fe673a235badabfc05620eb624ce29559d6
kent_condez <condezjohnkent@gmail.com>
2025-12-16 18:13:16 +0800

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import {Navbar} from "./components";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
         </div>
        </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
